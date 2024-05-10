c063bdee2cc2ecca2b4f8e686fd05f4e90ac1e39
SarraJEE <bgrsarra@gmail.com>
2024-05-10 23:16:42 +0200

    Ajout des experiences

--- data/skills.js
+++ data/skills.js
@@ -1,25 +1,25 @@
 export const skillData = [
   {
     id: 1,
-    title: "Web Design",
+    title: "",
     progress: 85,
     progressClass: "progress-bg-1",
   },
   {
     id: 2,
-    title: "Mobile App",
+    title: "",
     progress: 55,
     progressClass: "progress-bg-2",
   },
   {
     id: 3,
-    title: "Illustrator",
+    title: "",
     progress: 65,
     progressClass: "progress-bg-3",
   },
   {
     id: 4,
-    title: "Photoshop",
+    title: "",
     progress: 72,
     progressClass: "progress-bg-4",
   },
