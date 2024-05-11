8887493ebb711bb11ee4db3f6c8010fa2fb338f5
SarraJEE <bgrsarra@gmail.com>
2024-05-11 13:35:41 +0200

    Ajout  titre

--- app/page.jsx
+++ app/page.jsx
@@ -7,9 +7,9 @@ import Portfolio from "@/components/homes/portfolio/Portfolio";
 
 import React from "react";
 export const metadata = {
-  title: "Portfolio-1 || Bostami - Professional portfolio NextJS Template",
+  title: "Portfolio-1 || srlkmarine",
   description:
-    "Discover Botami,the most impressive portfolio template for work showcase, blog",
+    "",
 };
 export default function page() {
   return (
